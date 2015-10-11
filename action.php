64c38b2f5cfc78db1b4c897ee82a704fb9a91c6c
Yevhen L. <yevhen.lisovenko@gmail.com>
2015-10-11 14:30:54 +0200

    Update action.php

--- action.php
+++ action.php
@@ -1,19 +1,18 @@
 <?php 
 
-  include_once "Security.php";
-
-  if ($_SERVER['REQUEST_METHOD'] != 'POST'){
-      header("Location: /");
-      exit;
-  }
-
-  $security = new Security(false);
+	include_once "Security.php";
+	
+	if ($_SERVER['REQUEST_METHOD'] != 'POST'){
+		header("Location: /");
+		exit;
+	}
+	
+	$security = new Security(false);
 	$secret_key = $security->hashDecode($security->ReturnAccessHash(), 0);
 	$secret_value = $security->hashDecode($security->ReturnRandHash(), 1);
 	
 	if (isset($_POST[$secret_key]) && $_POST[$secret_key] == $secret_value){
-	    die('Protected');
+		die(json_encode(array("status" => "ok", 'msg' => 'Protected', "rh"=>$security->ReturnRandHash(), "ah"=>$security->ReturnAccessHash(), "sid"=>$security->ReturnSessionId())));
 	}
 	
-  die('Error access');	
-	
+	die('Error access');
